31add70ad55ff942b7e999d46d4636a89dc30548
Ezequiel Echeverri <ez.echeverri@gmail.com>
2021-06-18 03:07:36 -0300

    hover card juego

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -60,6 +60,10 @@ const JuegoDiv = styled(motion.div)`
     font-size: 1.8rem;
     color: #white;
   }
+  &:hover {
+    background: #6564DB;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);
+  }
 `;
 
 export default Juego;
