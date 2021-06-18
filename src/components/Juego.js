1f6febebf15544334e98b15b35f4098e050e30d3
Ezequiel Echeverri <ez.echeverri@gmail.com>
2021-06-18 03:16:38 -0300

    prueba agregar otro remote git

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -10,7 +10,7 @@ import { popup } from "../animation";
 const Juego = ({ name, image, released, id }) => {
   const dispatch = useDispatch();
   const stringPathId = id.toString();
-
+  //prueba commit
   const loadDetailHandler = () => {
     document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
@@ -64,6 +64,7 @@ const JuegoDiv = styled(motion.div)`
     background: #6564DB;
     box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);
   }
+
 `;
 
 export default Juego;
